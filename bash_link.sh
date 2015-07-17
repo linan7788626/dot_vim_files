2638cef8b0ffb78898a6ae07b35465e74ac7a69a
linan7788626 <linan7788626@gmail.com>
2015-07-17 11:47:12 -0600

    0.1 version

---
+++ bash_link.sh
@@ -0,0 +1,5 @@
+ln -sf /Users/uranus/GitHub/dot_vim_files/.vim	~/.vim
+ln -sf /Users/uranus/GitHub/dot_vim_files/.vimrc ~/.vimrc
+ln -sf /Users/uranus/GitHub/dot_vim_files/.vimrc.before	~/.vimrc.before
+ln -sf /Users/uranus/GitHub/dot_vim_files/.vimrc.bundles ~/.vimrc.bundles
+ln -sf /Users/uranus/GitHub/dot_vim_files/.vimrc.local	~/.vimrc.local
