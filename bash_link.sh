2db9072fe49a237d444979ee0031ad9835d9f0d8
Nan Li <nanli@cooleylogin2.fst.alcf.anl.gov>
2015-09-22 16:37:23 +0000

    update bash script

--- bash_link.sh
+++ bash_link.sh
@@ -1,5 +1,7 @@
-ln -sf /Users/uranus/GitHub/dot_vim_files/.vim	~/.vim
-ln -sf /Users/uranus/GitHub/dot_vim_files/.vimrc ~/.vimrc
-ln -sf /Users/uranus/GitHub/dot_vim_files/.vimrc.before	~/.vimrc.before
-ln -sf /Users/uranus/GitHub/dot_vim_files/.vimrc.bundles ~/.vimrc.bundles
-ln -sf /Users/uranus/GitHub/dot_vim_files/.vimrc.local	~/.vimrc.local
+mkdir ~/old.vim.copy
+mv ~/.vim* ~/old.vim.copy
+ln -sf `pwd -P`/.vim	~/.vim
+ln -sf `pwd -P`/.vimrc ~/.vimrc
+ln -sf `pwd -P`/.vimrc.before	~/.vimrc.before
+ln -sf `pwd -P`/.vimrc.bundles ~/.vimrc.bundles
+ln -sf `pwd -P`/.vimrc.local	~/.vimrc.local
